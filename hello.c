2fc1d4fd3511b1286bd1b4a330e2fbc7effbe75d
zymgp <571841501@qq.com>
2020-08-27 20:23:21 +0800

    Add tests for multiply function

--- hello.c
+++ hello.c
@@ -9,11 +9,18 @@ int multiply(int a, int b)
     return a * b;
 }
 
+int test()
+{
+    assert(multiply(2, 2) == 4);
+    assert(multiply(-3, 2) == 6);
+}
+
 int main()
 {
     // I'm writing somthing, but havn't finished yet!
     // TODO:
     
     printf("Hey, Little Ross!");
+    test();
     return 0;
 }
